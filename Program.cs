a0b88d803be54b77ddf4e59ab35612e92190943d
Erich Richter <erich.richter@gmail.com>
2020-09-25 08:15:37 -0700

    create initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pin_brawl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+starting research for pin-brawl game
